5f4bf2c5c8498566747378e3d64cf82dd3752533
yoshizu <yoshizu@YOSHIZU-AEROG11.cac-k.jp.local>
2026-04-16 14:53:50 +0900

    Question07_2 まで

--- java_comprehension_exercises_volume1/src/advanced/question07/Question07_3.java
+++ java_comprehension_exercises_volume1/src/advanced/question07/Question07_3.java
@@ -1,6 +1,8 @@
 package advanced.question07;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question07_3 {
 
@@ -12,9 +14,24 @@ public class Question07_3 {
 		int[] prices = { 100, 200, 300, 400, 500 };
 		// 購入数（変更不可）
 		int[] amounts = new int[5];
+		int[] stocks = { 3, 5, 2, 1, 4 };
 
-		// TODO: 実装ここから
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String str = reader.readLine();
+		int num = Integer.parseInt(str);
 
+		if (stocks[0] < num && stocks[1] < num && stocks[2] < num && stocks[3] < num && stocks[4] < num)
+			;
+		{
+			System.out.println("在庫を超えています。もう一度入力してください。");
+		}
 
+		System.out.println(itemNames[0] + ":" + prices[0] * amounts[0] + "円");
+		System.out.println(itemNames[1] + ":" + prices[1] * amounts[1] + "円");
+		System.out.println(itemNames[2] + ":" + prices[2] * amounts[2] + "円");
+		System.out.println(itemNames[3] + ":" + prices[3] * amounts[3] + "円");
+		System.out.println(itemNames[4] + ":" + prices[4] * amounts[4] + "円");
+		System.out.println("合計金額:" + ((prices[0] * amounts[0]) + (prices[1] * amounts[1]) + (prices[2] * amounts[2])
+				+ (prices[3] * amounts[3]) + (prices[4] * amounts[4])) + "円");
 	}
 }
